ead87a008a1e7f2c002a42e072ac704a008bce0e
kevinJV <163199@ids.upchiapas.edu.mx>
2020-07-22 10:27:09 -0500

    Product model created

---
+++ database/migrations/1595431091655_product_schema.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class ProductSchema extends Schema {
+  up () {
+    this.create('products', (table) => {
+      table.increments()
+      table.string('name')
+      table.string('image')
+      table.string('description')
+      table.decimal('price', 12, 2)
+      table.integer('quantity')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('products')
+  }
+}
+
+module.exports = ProductSchema
